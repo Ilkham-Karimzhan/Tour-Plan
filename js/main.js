60788816d5f95f12ee8022b38007e95e24057bf4
Ilkham-Karimzhan <ilhamkar00@gmail.com>
2021-03-17 15:13:21 +0600

    create: review section, fixed hotel-slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-const swiper = new Swiper('.swiper-container', {
+const hotelSwiper = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.slider__button--next',
-    prevEl: '.slider__button--pref',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--pref',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+const reviewSwiper = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
   keyboard: {
     enabled: true,
