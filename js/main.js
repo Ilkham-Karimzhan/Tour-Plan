7e2de5601e91efd4e30cbdf389186a553fd4478a
Ilkham-Karimzhan <ilhamkar00@gmail.com>
2021-03-23 19:07:51 +0600

    create: modal window

--- js/main.js
+++ js/main.js
@@ -26,15 +26,33 @@ $(document).ready(function(){
     },
   });
   
-  var menuButton = document.querySelector(".menu-button");
-  menuButton.addEventListener("click", function(){
-    console.log("Hello"); 
-    document.querySelector(".navbar__bottom").classList.toggle('navbar__bottom--vivible');
+  var menuButton = $(".menu-button");
+
+  menuButton.on("click", function(){
+    $(".navbar__bottom").toggleClass('navbar__bottom--vivible');
   });
   var modalButton = $('[data-toggle=modal]');
-  modalButton.on('click', function(){});
+  var closeModalButton = $('.modal__close');
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
   function openModal(){
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");      
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");   
+  }
+  function closeModal(event){
+    event.preventDefault();
     var modalOverlay = $('.modal__overlay');
     var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
   }
+  $(document).keyup(function(e) {
+    if (e.key === "Escape" || e.keyCode === 27) {
+      var modalOverlay = $('.modal__overlay');
+      var modalDialog = $('.modal__dialog');
+      modalOverlay.removeClass('modal__overlay--visible');
+      modalDialog.removeClass('modal__dialog--visible');
+    }
+  });
 });
