542858222ca66330b1d38dc4406e7b59bee2d50e
Ilkham-Karimzhan <ilhamkar00@gmail.com>
2021-03-23 17:34:27 +0600

    create: php form, mobile responsive

--- js/main.js
+++ js/main.js
@@ -1,32 +1,40 @@
-const hotelSwiper = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--pref',
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-const reviewSwiper = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function(){
-  console.log("Hello"); 
-  document.querySelector(".navbar__bottom").classList.toggle('navbar__bottom--vivible');
+$(document).ready(function(){
+  const hotelSwiper = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--pref',
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+  const reviewSwiper = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+  
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function(){
+    console.log("Hello"); 
+    document.querySelector(".navbar__bottom").classList.toggle('navbar__bottom--vivible');
+  });
+  var modalButton = $('[data-toggle=modal]');
+  modalButton.on('click', function(){});
+  function openModal(){
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+  }
 });
---
+++ send.php
@@ -0,0 +1,85 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+
+// Формирование самого письма
+if ($email == null) {
+    $title = "Новое обращение Best Tour Plan";
+    $body = "
+    <h2>Новое обращение</h2>
+    <b>Имя:</b> $name<br>
+    <b>Телефон  :</b> $phone<br><br>
+    <b>Сообщение:</b><br>$message
+    ";
+    $modal = "
+    <h2>Новое обращение</h2>
+    <b>Имя:</b> $name<br>
+    <b>Телефон  :</b> $phone<br><br>
+    <b>Почта клиента для рассылки новостей: $email</b><br>
+    <b>Сообщение:</b><br>$message
+    ";
+} else {
+    $title = "Новое обращение Best Tour Plan";
+    $body = "
+    <h2>Новое обращение</h2>
+    ";
+    $modal = "
+    <h2>Новое обращение</h2><br>
+    <b>Почта клиента для рассылки новостей: $email</b>";
+}
+
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'ilkham1Karimov@gmail.com'; // Логин на почте
+    $mail->Password   = 'Gmail_mygmail_131199'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('ilkham1Karimov@gmail.com', 'Ильхам Каримов'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('ilhamkar00@gmail.com');  
+
+// Отправка сообщения
+if ($email == null) {
+  $mail->isHTML(true);
+  $mail->Subject = $title;
+  $mail->Body = $body;
+} elseif($email != null and $phone != null) {
+  $mail->isHTML(true);
+  $mail->Subject = $title;
+  $mail->Body = $modal;
+} elseif(($email != null and $phone == null)) {
+  $mail->isHTML(true);
+  $mail->Subject = $title;
+  $mail->Body = $sendMail;
+}
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header ('Location: thankyou.html');
